eec455d09247200e258ab167afe199e7ec395ead
stepmark136 <stepovojmakar2@gmail.com>
2026-09-17 10:49:21 +0300

    Lab01: system info investigations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
 Console.WriteLine($"Пользователь: {Environment.UserName}");
-Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM, уууу HH: mm3}");
+Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.уууу HH:mm}");
+
+Console.WriteLine($"OC: {Environment.OSVersion}");
+Console.WriteLine($"64-битная OC: {Environment.Is64BitOperatingSystem}");
+
+Console.WriteLine($"Логических процессоров: {Environment.ProcessorCount}");
+
+
+Console.WriteLine($"PID процесса: {Environment.ProcessId}");
+long memory = Environment.WorkingSet / 1024 / 1024;
+Console.WriteLine($"Память процесса: {memory} МБ");
+Console.ReadLine();
